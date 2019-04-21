02968ca1db644c6b1117dcf697affa28a173437f
mindscan-de <maxim.gansert@gmail.com>
2019-04-22 00:12:52 +0200

    refactored simple cache implementation.

--- src/de/mindscan/furiousiron/indexer/Cache.java
+++ src/de/mindscan/furiousiron/indexer/Cache.java
@@ -31,53 +31,98 @@ import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
 /**
- * 
+ * This class implements a simple document cache, for the original documents. The original file structure is 
+ * not preserved, instead it is built from the document id. The file structure / file name and so on, should 
+ * be obtained from the metadata / metainformation of the document. 
  */
 public class Cache {
+
+    /**
+     * folder in index Folder, where the 'downloaded' documents should be cached. 
+     */
+    private static final String CACHED_DOCUMENTS_FOLDER = "cachedDocuments";
 
     /**
      * file suffix for files containing copies of the original content in the cache folder
      */
     public final static String CACHED_FILE_SUFFIX = ".originalContent";
 
-    /**
-     * file suffix for files containing unique word list of the document in the cache folder
-     */
-    public final static String WORDLIST_FILE_SUFFIX = ".wordlist";
+//    /**
+//     * file suffix for files containing unique word list of the document in the cache folder
+//     */
+//    public final static String WORDLIST_FILE_SUFFIX = ".wordlist";
 
     // private Path indexFolder;
     private Path cacheFolder;
 
+    /**
+     * C'tor 
+     * @param indexFolder path of folder, where to store the cached Documents. The documents 
+     */
     public Cache( Path indexFolder ) {
         // this.indexFolder = indexFolder;
-        this.cacheFolder = indexFolder.resolve( "cachedDocuments" );
+        this.cacheFolder = indexFolder.resolve( CACHED_DOCUMENTS_FOLDER );
     }
 
+    /**
+     * Create a copy of the content of the file given via fileToIndex. The content itself is 
+     * stored in a file accessible by the documentId. A file with the same documentId will
+     * overwrite (replace) the former file content.  
+     * 
+     * @param documentId the documentId which this document should be stored in the cache
+     * @param fileToIndex the path to the document to store
+     */
     public void createDocumentCopy( DocumentId documentId, Path fileToIndex ) {
-        String firstLayer = documentId.getMd5hex().substring( 0, 2 );
-        String secondLayer = documentId.getMd5hex().substring( 2, 4 );
+        // TODO: create cache directory structure beforehand.
+        // can be enforced completely beforehand, so this calculation doesn't need 
+        // to be part of each document copy operation 
+        // it would be simple to create either 256 or 65536 folders / shards beforehand
+        createTargetDirectoryIfNotExist( documentId );
 
-        Path targetPath = getTargetPath( firstLayer, secondLayer );
-        if (!Files.isDirectory( targetPath )) {
-            try {
-                Files.createDirectories( targetPath );
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        Path targetFile = targetPath.resolve( documentId.getMd5hex() + CACHED_FILE_SUFFIX );
         try {
-            Files.copy( fileToIndex, targetFile, StandardCopyOption.REPLACE_EXISTING );
+            Files.copy( fileToIndex, getTargetFilePath( documentId ), StandardCopyOption.REPLACE_EXISTING );
         }
         catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private Path getTargetPath( String firstLayer, String secondLayer ) {
-        return cacheFolder.resolve( firstLayer ); // .resolve( secondLayer );
+    /**
+     * 
+     * @param documentId
+     * @return
+     */
+    public Path getDocumentPathById( DocumentId documentId ) {
+        return getTargetFilePath( documentId );
+    }
+
+    // -----------------------
+    // Implementation details.
+    // -----------------------    
+
+    private void createTargetDirectoryIfNotExist( DocumentId documentId ) {
+        Path targetDirectoryPath = getTargetDirectoryPath( documentId );
+        if (!Files.isDirectory( targetDirectoryPath )) {
+            try {
+                Files.createDirectories( targetDirectoryPath );
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private Path getTargetFilePath( DocumentId documentId ) {
+        return getTargetDirectoryPath( documentId ).resolve( documentId.getMd5hex() + CACHED_FILE_SUFFIX );
+    }
+
+    private Path getTargetDirectoryPath( DocumentId documentId ) {
+        String firstLayer = documentId.getMd5hex().substring( 0, 2 );
+
+        // TODO: if we are exceeding 4k files per directory (means 1 mio files in total to index, we should enable the second layer. 
+        // String secondLayer = documentId.getMd5hex().substring( 2, 4 );
+
+        return cacheFolder.resolve( firstLayer ); //.resolve(secondLayer)
     }
 
 }
