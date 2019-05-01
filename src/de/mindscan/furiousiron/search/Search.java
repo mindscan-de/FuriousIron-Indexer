5a1719aa3abe93150eb51c40cf9fc8b113622d76
mindscan-de <maxim.gansert@gmail.com>
2019-05-01 11:28:46 +0200

    add search skeleton. search will later be moved to different project

---
+++ src/de/mindscan/furiousiron/search/Search.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ * MIT License
+ *
+ * Copyright (c) 2019 Maxim Gansert, Mindscan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+package de.mindscan.furiousiron.search;
+
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.Collections;
+
+import de.mindscan.furiousiron.index.cache.DocumentCache;
+import de.mindscan.furiousiron.index.cache.MetadataCache;
+import de.mindscan.furiousiron.index.cache.WordlistCache;
+import de.mindscan.furiousiron.index.trigram.InverseTrigramIndex;
+
+/**
+ * 
+ */
+public class Search {
+
+    // for content
+    private DocumentCache theFileCache;
+    // for ranking
+    private MetadataCache theMetadataCache;
+    // for ranking the results
+    private WordlistCache theWordlistCache;
+    // for ranking
+    private InverseTrigramIndex theInverseTrigramIndex;
+
+    /**
+     * @param indexFolder The folder, where the index root is located
+     * 
+     */
+    public Search( Path indexFolder ) {
+        theFileCache = new DocumentCache( indexFolder );
+        theMetadataCache = new MetadataCache( indexFolder );
+        theWordlistCache = new WordlistCache( indexFolder );
+        theInverseTrigramIndex = new InverseTrigramIndex( indexFolder );
+    }
+
+    /**
+     * @param searchterm the searchterm to search for in the index.
+     * @return the collection of documents
+     */
+    public Collection<SearchResultCandidates> search( String searchterm ) {
+        return Collections.emptyList();
+    }
+}
