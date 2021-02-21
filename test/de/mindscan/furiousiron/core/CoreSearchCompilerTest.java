dc25ae90464ced1d4fdfa0e926c08cc0d6d9960b
mindscan-de <maxim.gansert@gmail.com>
2021-02-21 11:12:45 +0100

    refactor: move CoreSearchCompiler to the indexer project

---
+++ test/de/mindscan/furiousiron/core/CoreSearchCompilerTest.java
@@ -0,0 +1,38 @@
+package de.mindscan.furiousiron.core;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.Test;
+
+import de.mindscan.furiousiron.core.ast.CoreQueryNode;
+import de.mindscan.furiousiron.core.ast.EmptyCoreNode;
+import de.mindscan.furiousiron.query.ast.EmptyNode;
+
+public class CoreSearchCompilerTest {
+
+    @Test
+    public void testCompile_inputTreeIsNull_expectEmptyCoreNode() {
+        // arrange
+
+        // act
+        CoreQueryNode result = CoreSearchCompiler.compile( null );
+
+        // assert
+        assertThat( result, is( instanceOf( EmptyCoreNode.class ) ) );
+
+    }
+
+    @Test
+    public void testCompile_inputTreeIsEmptyNode_expectEmptyCoreNode() {
+        // arrange
+
+        // act
+        CoreQueryNode result = CoreSearchCompiler.compile( new EmptyNode() );
+
+        // assert
+        assertThat( result, is( instanceOf( EmptyCoreNode.class ) ) );
+    }
+
+}
